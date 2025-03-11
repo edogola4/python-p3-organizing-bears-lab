84ba98776d46de8de756fe0345b0bd28c8d7c519
edogola4 <edogola4@gmail.com>
2025-03-11 17:24:42 +0300

    all test are passing

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 20, 'M', 'Brown', 'Calm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 10, 'M', 'Black', 'Rowdy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 6, 'F', 'Gray', 'Playful', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 15, 'M', 'Brown', 'Serious', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 13, 'F', 'White', 'Friendly', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 2, 'M', 'Green', 'Mischievous', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 6, 'F', 'Blonde', 'Gentle', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 8, 'F', 'Black', 'Curious', 1);
